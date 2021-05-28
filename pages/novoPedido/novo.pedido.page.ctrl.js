6e2a62ef4fcbe865154e76c42e203c6b0f0de293
Joao Pedro Ronzani dos Santos <joao.ronzani@betha.com.br>
2021-05-28 12:00:40 -0300

    sistema de pesquisa com dados mockados iniciado

--- pages/novoPedido/novo.pedido.page.ctrl.js
+++ pages/novoPedido/novo.pedido.page.ctrl.js
@@ -1,4 +1,33 @@
 angular.module("sistemaManutencao").controller("novoPedidoPageCtrl", function ($scope, $location) {
 	
+
+    $scope.clientes=[{
+        nome:"joao"
+    },{
+        nome:"pedro"
+    }]
+    $scope.clientesFiltrados=$scope.clientes;
+    $scope.nome=""
+    
+    $scope.show=false;
+    
+    
+    $scope.selecionar=function(nome) {
+        $scope.nome=nome;
+    }
+    
+    $scope.toggle=function() {
+        $scope.show=true;
+      }
+      
+    $scope.filtrar=function (palavra) {
+        console.log(palavra)
+        $scope.clientesFiltrados=[]
+        for(let i=0;i<$scope.clientes.length;i++){
+            if($scope.clientes[i].nome.includes(palavra)){
+                $scope.clientesFiltrados[i]=$scope.clientes[i];
+            }
+        }
+      }
 	
 });
